5588e3f48d6ec3fb7dfc42f3f4b5b28be9cfef92
Shampyn <31312215+Shampyn@users.noreply.github.com>
2019-10-23 15:47:49 +0300

    Add TracerLibrary + fix tracer

--- MPP.Tracer/Program.cs
+++ MPP.Tracer/Program.cs
@@ -9,13 +9,29 @@ namespace MPP.Tracer
 {
     class Program
     {
-        public static void TestMe()
+        public static void TestMe(Tracer tracer)
         {
+            tracer.StartTrace();
+            int z = 100;         
+                for (int i = 1; i < 10000; i++)
+                {
+                    i.ToString();
+                    z = z - i;
+                }
+            
+            tracer.StopTrace();
+        }
+
+        public static void TestMe1(Tracer tracer)
+        {
+            tracer.StartTrace();
             int z = 100;
-            for (int i = 1; i < 100; i++)
+            for (int i = 1; i < 10000; i++)
             {
+                i.ToString();
                 z = z - i;
             }
+            tracer.StopTrace();
         }
 
 
@@ -23,10 +39,31 @@ namespace MPP.Tracer
         {
             Tracer tracer = new Tracer();
             tracer.StartTrace();
-            TestMe();
+            TestMe(tracer);
+            TestMe(tracer);
+            Thread thread1 = new Thread(() => { TestMe1(tracer); });
+            thread1.Start();
             Thread.Sleep(100);
             tracer.StopTrace();
-    
+
+            FileTraceRusultWriter fw = new FileTraceRusultWriter("Result.xml");
+
+
+            XMLSerializer xml = new XMLSerializer();
+            fw.WriteResult(xml.Serialize(tracer.GetTraceResult()));
+
+           
+
+            ConsoleTraceResultWriter rw = new ConsoleTraceResultWriter();
+
+
+            XMLSerializer xml1 = new XMLSerializer();
+            rw.WriteResult(xml.Serialize(tracer.GetTraceResult()));
+
+            JSONSerializer json = new JSONSerializer();
+            rw.WriteResult(json.Serialize(tracer.GetTraceResult()));
+            Console.ReadLine();
+
         }
     }
 }
---
+++ TracerLibrary/ConcurrentSerializableDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace MPP.Tracer
+{
+    public class ConcurrentSerializableDictionary<TKey, TValue> : ConcurrentDictionary<TKey, TValue>, IXmlSerializable
+    {
+      
+        public void ReadXml(XmlReader reader)
+        {
+            Console.WriteLine("Not implemented");
+        }
+
+        public XmlSchema GetSchema()
+        {
+            Console.WriteLine("Not implemented");
+            return null;
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            XmlSerializer ValueSerializer = new XmlSerializer(typeof(TValue));
+            foreach (TKey key in Keys)
+            {
+                TValue value = this[key];
+                ValueSerializer.Serialize(writer, value);
+            }
+        }
+
+      
+    }
+}
